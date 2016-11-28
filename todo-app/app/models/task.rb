0d56eb10467a35f492c67cbab42fb9fc24b631c8
Esmeralda Cervantes <ecervant@nd.edu>
2016-11-28 17:50:08 -0500

    validations added

--- todo-app/app/models/task.rb
+++ todo-app/app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ActiveRecord::Base
   belongs_to :task_list
+  validates :name, :priority, :due_date, :task_list_id, presence: true
+  validates :priority, inclusion: { in: 1..10, message: "Priority must be between 1 and 10"}
   def to_s
     name
   end
--- todo-app/app/models/task_list.rb
+++ todo-app/app/models/task_list.rb
@@ -1,5 +1,8 @@
 class TaskList < ActiveRecord::Base
   has_many :tasks
+  validates :name, :description , presence: true
+  validates :name, uniqueness: true
+
   def to_s
     name
   end
