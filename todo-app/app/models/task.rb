4e762cee9db60163820cc1cab139e26745644529
Esmeralda Cervantes <ecervant@nd.edu>
2016-11-28 23:09:17 -0500

    homework assignment due 11/29 complete

--- todo-app/app/models/task.rb
+++ todo-app/app/models/task.rb
@@ -1,7 +1,12 @@
 class Task < ActiveRecord::Base
   belongs_to :task_list
   validates :name, :priority, :due_date, :task_list_id, presence: true
-  validates :priority, inclusion: { in: 1..10, message: "Priority must be between 1 and 10"}
+  validates :priority, inclusion: { in: 1..10, message: "\"%{value}\" must be between 1 and 10"}
+
+scope :completed, -> {where(is_completed: true).order(created_at: :desc)}
+scope :pending, -> {where(is_completed: false).order(created_at: :asc)}
+
+
   def to_s
     name
   end
